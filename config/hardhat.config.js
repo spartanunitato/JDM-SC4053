41b358763f2a524d714eaef427fddddd962362b3
Magnus Lim <64314278+spartanunitato@users.noreply.github.com>
2024-10-23 21:33:56 +0800

    Add files via upload

---
+++ config/hardhat.config.js
@@ -0,0 +1,16 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+// hardhat.config.js
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
